3c677fe801a4962cc8e621bb0d236bc773e53ad3
Gerry Morales Meza <sleep.tiger2@gmail.com>
2017-12-12 10:41:28 -0600

    Updated first activity

--- Object2.js
+++ Object2.js
@@ -5,3 +5,13 @@ const adventurer = {
 }
 
 console.log(adventurer.belongings[2]);
+
+adventurer.belongings.push("Vogue Magazine");
+
+function findBelongings () {
+	for (i = 0; i < adventurer.belongings.length; i++) {
+		console.log(adventurer.belongings[i])
+	}
+}
+
+findBelongings();
